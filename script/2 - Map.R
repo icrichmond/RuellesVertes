f41791414c821203c7d488f92b26007dd5d45b4a
icrichmond <icrichmond@mun.ca>
2021-06-07 14:41:35 -0400

    Script for mapping

---
+++ script/2 - Map.R
@@ -0,0 +1,9 @@
+# Ruelles Vertes Site Selection 
+# Authors: Isabella Richmond & Emily Kroft 
+
+#### Load Packages ####
+easypackages::packages("sf", "tidyverse")
+
+#### Load Data ####
+
+#### Map ####
