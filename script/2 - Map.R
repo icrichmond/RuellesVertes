c61daca92f8f9ad5f1d10765dbf1057c8d51bae0
icrichmond <icrichmond@mun.ca>
2021-06-07 15:01:35 -0400

    Investigating our layers with mapview

--- script/2 - Map.R
+++ script/2 - Map.R
@@ -2,8 +2,23 @@
 # Authors: Isabella Richmond & Emily Kroft 
 
 #### Load Packages ####
-easypackages::packages("sf", "tidyverse")
+easypackages::packages("sf", "tidyverse", "mapview")
 
 #### Load Data ####
+# Ruelles Vertes
+rv_pr <- readRDS("output/RuellesRosemontPlateau.rds")
+# All roads
+rds <- readRDS("output/RoadsRosemontPlateau.rds")
+# Parks
+parcs <- readRDS("output/ParksRosemontPlateau.rds")
+# Major roads
+mjrds <- readRDS("output/MajorRoadsRosemontPlateau.rds")
+
+#### Mapview ####
+# we can use mapview to investigate our spatial data like we would in Arc or Q
+mapview(mjrds)
+mapview(parcs)
+mapview(rv_pr)
 
 #### Map ####
+# ggplot works well for mapping sf objects
