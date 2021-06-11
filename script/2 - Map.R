1b209f2eb4170197304c9635a95615c934460fab
icrichmond <icrichmond@mun.ca>
2021-06-11 14:33:43 -0400

    Rosemont map

--- script/2 - Map.R
+++ script/2 - Map.R
@@ -6,15 +6,17 @@ easypackages::packages("sf", "tidyverse", "mapview", "ggspatial")
 
 #### Load Data ####
 # Ruelles Vertes
-rv_pr <- readRDS("output/RuellesRosemontPlateau.rds")
+rv_r <- readRDS("output/RuellesRosemont.rds")
 # All roads
 rds <- readRDS("output/RoadsRosemontPlateau.rds")
 # Parks
-parcs <- readRDS("output/ParksRosemontPlateau.rds")
+parcs <- readRDS("output/ParksRosemont.rds")
 # Major roads
-mjrds <- readRDS("output/MajorRoadsRosemontPlateau.rds")
+mjrds <- readRDS("output/MajorRoadsRosemont.rds")
 # Final ruelles
 lns_ss <- readRDS("output/FinalRuelles.rds")
+# Sampling points
+spt <- readRDS("output/ruellessitesID.rds")
 
 #### Mapview ####
 # we can use mapview to investigate our spatial data like we would in Arc or Q
@@ -25,12 +27,13 @@ mapview(lns_ss)
 
 #### Map ####
 ggplot() + 
-  geom_sf(data = rv_pr, col = "black") + 
+  geom_sf(data = rv_r, col = "black") + 
   geom_sf(data = parcs, fill = "palegreen4") + 
   geom_sf(data = mjrds,  col = "gray44") + 
+  #geom_sf(data = spt, col = "lightblue", size = 0.5)
   coord_sf(crs = "+init=epsg:6624") + 
-  annotation_scale(location = "bl", width_hint = 0.5) +
-  annotation_north_arrow(location = "br", which_north = "true", style = north_arrow_fancy_orienteering) +
+  annotation_scale(location = "tl", width_hint = 0.5) +
+  annotation_north_arrow(location = "tl", which_north = "true", style = north_arrow_fancy_orienteering) +
   theme(panel.grid.major = element_line(color = gray(.1), linetype = "dashed", size = 0.1),
         panel.background = element_rect(fill = NA))
-ggsave("graphics/AllRuelles_MjrRds_Parcs_RosemontPlateau.jpg",dpi = 400)
+ggsave("graphics/AllRuelles_MjrRds_Parcs_Rosemont.jpg",dpi = 400)
