dbbc166145aec941d169078d267c84ca1e40ddd0
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2022-03-10 12:12:11 -0500

    specify use in GEE

--- script/B0-VectorizeCanopy.js
+++ script/B0-VectorizeCanopy.js
@@ -1,6 +1,6 @@
 // 'cc' in this script refers to the canopy data that was sourced from http://observatoire.cmm.qc.ca/observatoire-grand-montreal/produits-cartographiques/donnees-georeferencees/
 // the canopy data was loaded into GEE as an asset and then worked with from there 
-// the canopy data is a raster layer and this code vectorized it so it could be used in scripts B1-B3
+// the canopy data is a raster layer and this code vectorized it using Google Earth Engine so it could be used in scripts B1-B3
 
 var landcover = cc.select('b1');
 
