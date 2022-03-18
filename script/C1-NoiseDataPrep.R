0518fd412edbe5eb826b14566f93111304520ef9
icrichmond <icrichmond@mun.ca>
2022-03-18 13:52:42 -0400

    format file names + times

--- script/C1-NoiseDataPrep.R
+++ script/C1-NoiseDataPrep.R
@@ -8,15 +8,24 @@ source("R/RenamingFiles.R")
 # Renaming Data -------------------------------------
 # noise data is extremely large - stored on a separate drive 
 # there is a separate folder for each ruelle with .wav files named by date-time 
-# need .wav files to have ruelle ID in name 
-rename_batch("D:/Ruelles-Vertes/", "WAV")
+# need .wav files to have ruelle ID in name
+# (ONLY RUN ONCE)
+folders <- as.list(list.files(path = "D:/Ruelles-Vertes"))
+lapply(folders, function(x) rename_batch(paste0("D:/Ruelles-Vertes/", x), "WAV"))
 
-# Filtering Data --------------------------------------
-# need to filter each folder (1 for each Ruelle ID) for common dates 
-# 18-08-2021 to 05-09-2021
+# Formatting Data -----------------------------------
+# get all .wav file details 
+details <- file.info(dir(path = "D:/Ruelles-Vertes/", pattern = ".WAV", recursive = T, full.names = TRUE), extra_cols = FALSE)
+# add file names as a column without the full path
+details$filenames <- list.files(path = "D:/Ruelles-Vertes", pattern = ".WAV", recursive = T, full.names = FALSE, include.dirs = FALSE)
+details$filenames <- sub(".*?\\/", "", details$filenames)
+# select just the part of the name that contains the date/time 
+details$starttimes <- details$filenames |> (\(.)  sub(".*?\\/", "", .))() |> (\(.)  gsub("\\-.*", "", .))()
+# convert into dates and times 
+details$starttimes <- as.POSIXct(details$starttimes, format = '%Y%m%d_%H%M%S', tz = 'UTC')
 
-# get file details 
-details = file.info(list.files(path = "D:/Ruelles-Vertes/", pattern="*.WAV"))
-details = details[with(details, order(as.POSIXct(mtime))), ]
-files = rownames(details)
-# select files within specified time frame and write to new folder 
+# Filtering Data -------------------------------------
+# need to filter files for common dates 
+# 2021-08-18 to 2021-09-05
+
+
