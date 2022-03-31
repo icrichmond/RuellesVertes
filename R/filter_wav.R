ab8bec1311d4ac6d45f6592b595a04e5f8af113a
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-31 14:03:33 -0700

    add check for class min/max date and DT's date

--- R/filter_wav.R
+++ R/filter_wav.R
@@ -1,7 +1,11 @@
 #' @param DT
 #' @param min_date
 #' @param max_date
-filter_wav <- function(DT, min_date = min_date, max_date = max_date) {
+filter_wav <- function(DT, min_date, max_date) {
+  if (class(min_date) != class(DT$date) | class(max_date) != class(DT$date)) {
+    stop('check class of min/max date and date column in DT')
+  }
+  
   DT[!is.na(time_cat) &
        between(date, min_date, max_date)]
 }
