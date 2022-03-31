c35b2f6f83ef8ce7839757cd8ec7d6a1679abdde
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-31 13:43:50 -0700

    parse date + time

--- R/get_wav_paths.R
+++ R/get_wav_paths.R
@@ -6,5 +6,10 @@ get_wav_paths <- function(path) {
   DT[, filename := basename(path)]
   DT[, folder := tstrsplit(path, '/')[[3]]]
   
-  DT[, datetime := parse_date(tstrsplit(filename, '_')[[1]])]
+  DT[, datetime_char := tstrsplit(filename, '-')[[1]]]
+  DT[, date := parse_date(tstrsplit(datetime_char, '_')[[1]])]
+  DT[, time := as.ITime(tstrsplit(datetime_char, '_')[[2]],
+                        format = '%H%M%S')]
+  
+  return(DT)
 }
