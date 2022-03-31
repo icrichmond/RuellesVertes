524fa9c7b8b2de8c5e5aacc7d6b89ae6640d679c
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-31 13:22:45 -0700

    fst packages

---
+++ R/packages.R
@@ -0,0 +1,12 @@
+# Packages ----------------------------------------------------------------
+library(targets)
+library(tarchetypes)
+
+library(data.table)
+
+library(tuneR)
+library(seewave)
+
+# This package helps detect function conflicts
+library(conflicted)
+
