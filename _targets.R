0cbddd350a96e28024564b451af97bfc0a1f5970
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-31 13:34:07 -0700

    draft get, cat, filter wav

--- _targets.R
+++ _targets.R
@@ -13,12 +13,35 @@ tar_option_set(format = 'qs',
 
 
 # Variables ---------------------------------------------------------------
+# TODO: fix this to your drive
+folder_path <- file.path('input', 'Ruelles-Vertes')
+
+min_date <- as.Date('2021-08-15')
+max_date <- as.Date('2021-08-22')
 
 
 
-
-# Paths -------------------------------------------------------------------
-
+# Targets: Noise ----------------------------------------------------------
+c(
+  tar_target(
+    wav_paths,
+    get_wav_paths(folder_path)
+  ),
+  tar_target(
+    cat_wav,
+    categorize_wav(
+      wav_paths
+    )
+  ),
+  tar_target(
+    filter_wav,
+    filter_wav(
+      cat_wav,
+      min_date = min_date,
+      max_date = max_date,
+    )
+  )
+)
 
 
 
