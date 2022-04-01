3102daf6cd737264932bad850c5773d664ef5b3c
icrichmond <icrichmond@mun.ca>
2022-04-01 14:40:30 -0400

    consolidating all targets here instead of just noise

--- _targets.R
+++ _targets.R
@@ -1,5 +1,5 @@
 # === Targets -------------------------------------------------------------
-
+# Alec Robitaille & Isabella Richmond
 
 
 # Source ------------------------------------------------------------------
@@ -11,59 +11,28 @@ tar_option_set(format = 'qs')
 
 
 # Variables ---------------------------------------------------------------
+
+# Sampling 
+
+
+
+# Temperature
+
+
+
+# Noise 
+
 folder_path <- file.path('D:', 'Ruelles-Vertes/')
 
-# TODO: fix these dates
+## note: change these dates as needed for different analysis
 min_date <- as.POSIXct('2021-08-10')
 max_date <- as.POSIXct('2021-08-22')
 
 
-
-# Targets: Noise ----------------------------------------------------------
-targets_noise <- c(
-  tar_target(
-    wav_files,
-    dir(folder_path, full.names = TRUE, recursive = TRUE),
-    format = 'file'
-  ),
-  tar_target(
-    wav_paths,
-    parse_wav_paths(wav_files)
-  ),
-  tar_target(
-    wav_categorized,
-    categorize_wav(
-      wav_paths
-    )
-  ),
-  tar_target(
-    wav_filtered,
-    filter_wav(
-      wav_categorized,
-      min_date = min_date,
-      max_date = max_date
-    )
-  ),
-  tar_target(
-    wav_noise,
-    calc_noise(wav_filtered),
-    pattern = map(wav_filtered)
-  ),
-  tar_target(
-    merge_noise,
-    merge(
-      wav_noise,
-      wav_filtered,
-      by = 'path'
-    )
-  ),
-  tar_target(
-    summ_noise, 
-    summarise_noise(merge_noise)
-  )
-)
-
-
+# Analysis ----------------------------------------------------------------
+source(file.path('script', 'sampling-selection-targets.R'))
+source(file.path('script', 'temperature-analysis-targets.R'))
+source(file.path('script', 'noise-analysis-targets.R'))
 
 
 # Targets: all ------------------------------------------------------------
