5d1b0a25187c59008d1a82bf03e7dbe2fba83c59
icrichmond <icrichmond@mun.ca>
2022-04-01 15:55:59 -0400

    transforming scripts A1 - A3 to targets

---
+++ script/sampling-selection-targets.R
@@ -0,0 +1,62 @@
+
+# Targets: Site Selection ----------------------------------------------------------
+targets_clean <- c(
+  tar_target(
+    ruelles_data,
+    read_sf("input/ruelles/ruelles-vertes.shp")
+  ),
+  tar_target(
+    parks_data,
+    read_sf("input/parcs/Espace_Vert.shp")
+  ),
+  tar_target(
+    roads_data,
+    read_sf("input/roads/road_segment_1.shp")
+  ),
+  tar_target(
+    sample_pts, 
+    read_sf("output/sampling_points/ruellessites.shp")
+  ),
+  tar_target(
+    ruelles_filtered,
+    filter_ruelles(ruelles_data)
+  ),
+  tar_target(
+    parks_cleaned,
+    clean_parks(ruelles_filtered, parks_data)
+  ),
+  tar_target(
+    roads_cleaned, 
+    clean_roads(ruelles_filtered, roads_data)
+  ),
+  tar_target(
+    ruelles_outliers,
+    clean_outliers(ruelles_filtered, parks_cleaned)
+  ),
+  tar_target(
+    final_sites, 
+    sites_final(ruelles_cleaned, sample_pts)
+  ),
+  tar_targets(
+    map, 
+    plot_map(ruelles_filtered, parks_cleaned, roads_cleaned, final_sites)
+  )
+    
+)
+
+
+targets_grouping <- c(
+  tar_target(
+    grp, 
+    "input/RuellesVertes_Groups.csv",
+    format = "file"
+  ),
+  tar_target(
+    ruelles_kml, 
+    st_read("output/ruelles-sampling/ruellessitesID.kml")
+  ),
+  tar_target(
+    sites_group,
+    group_sites(grp, ruelles_kml)
+  )
+)
