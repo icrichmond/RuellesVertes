dc466fe0595c07aac25a25558daa4833f04f20fb
icrichmond <icrichmond@mun.ca>
2022-03-31 20:33:13 -0400

    add function for data summary

---
+++ R/summarise_noise.R
@@ -0,0 +1,7 @@
+#' @param path path to wav folder dir (containing subfolders)
+summarise_noise <- function(DT) {
+  n <- c("meanACI", "sdACI", "rangeACI", "meanNDSI", "sdNDSI", "rangeNDSI")
+  
+  DT[, (n):= .(mean(ACI), sd(ACI), list(range(ACI)), mean(NDSI), sd(NDSI), list(range(NDSI))),
+     by = .(folder, time_cat)]
+}
