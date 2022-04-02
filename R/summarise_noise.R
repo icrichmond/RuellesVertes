15527acc6977cb526d4e3ccd9fe6061e22951875
icrichmond <icrichmond@mun.ca>
2022-04-02 16:05:33 -0400

    change range to min/max columns

--- R/summarise_noise.R
+++ R/summarise_noise.R
@@ -1,10 +1,9 @@
 #' @param path path to wav folder dir (containing subfolders)
 summarise_noise <- function(DT) {
-  n <- c("meanACI", "sdACI", "rangeACI", "meanNDSI", "sdNDSI", "rangeNDSI")
+  n <- c("meanACI", "sdACI", "minACI", "maxACI", "meanNDSI", "sdNDSI", "minNDSI", "maxNDSI")
   
-  DT[, (n):= .(mean(ACI), sd(ACI), list(range(ACI)), mean(NDSI), sd(NDSI), list(range(NDSI))),
+  DT[, (n):= .(mean(ACI), sd(ACI), min(ACI), max(ACI), mean(NDSI), sd(NDSI), min(NDSI), max(NDSI)),
      by = .(folder, time_cat)]
   
-  DT$rangeACI <- as.character(DT$rangeACI)
-  DT$rangeNDSI <- as.character(DT$rangeNDSI)
+  
 }
