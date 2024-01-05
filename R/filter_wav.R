8fd30ee45a1905cdd0ea862bea39659c46b66e3d
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-01-05 12:04:02 -0500

    fix code not working

--- R/filter_wav.R
+++ R/filter_wav.R
@@ -2,10 +2,9 @@
 #' @param min_date
 #' @param max_date
 filter_wav <- function(DT, min_date, max_date) {
-  if (class(min_date) != class(DT$date) | class(max_date) != class(DT$date)) {
-    stop('check class of min/max date and date column in DT')
+  if (class(min_date)[1] != class(DT$date)[1] | class(max_date)[1] != class(DT$date)[1]){ 
+         stop('check class of min/max date and date column in DT')
   }
   
-  DT[!is.na(time_cat) &
-       between(date, min_date, max_date)]
+  DT[!is.na(time_cat) & between(date, min_date, max_date)]
 }
