bebe1cea86b3c2bc0423aaf09707e77c2ea6536a
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-01-05 12:03:56 -0500

    add time zone

--- _targets.R
+++ _targets.R
@@ -17,8 +17,8 @@ tar_option_set(format = 'qs')
 folder_path <- file.path('D:', 'Ruelles-Vertes/')
 
 ## note: change these dates as needed for noise different analysis
-min_date <- as.POSIXct('2021-08-10')
-max_date <- as.POSIXct('2021-08-22')
+min_date <- as.POSIXct('2021-08-10', tz = "UTC")
+max_date <- as.POSIXct('2021-08-22', tz = "UTC")
 
 
 # Analysis ----------------------------------------------------------------
