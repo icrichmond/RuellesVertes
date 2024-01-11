b2aaa91406a79bde8603cef8e52b6f499a59def6
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-01-11 12:19:39 -0500

    only 50 and 100 m buffer, remove roads

--- R/model_data.R
+++ R/model_data.R
@@ -11,10 +11,8 @@ model_data <- function(temp_plot, rv, var){
   rv.vars <- rv %>% 
     select(c("RuelleID", "Food_coverage_per_m2", "Floral_coverage_per_m2",
              "mean_NDSI", "Avg_max_daily_temp", "Avg_min_daily_temp",
-             "X50m_road_buffer" ,"X60m_road_buffer", "X70m_road_buffer" ,
-             "X80m_road_buffer", "X90m_road_buffer" ,"X100m_road_buffer" ,
-             "X200m_road_buffer", "X300m_road_buffer" ,"X400m_road_buffer" ,
-             "X500m_road_buffer", "Ruelle_length_m","Ruelle_area_m2", "Groundcover_avg", "Midstorey_avg", "Canopy_avg",
+             "X50m_road_buffer", "X100m_road_buffer", "Ruelle_length_m","Ruelle_area_m2", 
+             "Groundcover_avg", "Midstorey_avg", "Canopy_avg",
              "PublicArt", "PlayEquipment","WildlifeSupport","MaintainedGardens","StructureCondition")) %>%
     mutate_if(is.character, as.factor)
   rv.vars$RuelleID <- gsub("_", "-", rv.vars$RuelleID)
@@ -24,15 +22,17 @@ model_data <- function(temp_plot, rv, var){
   full <- left_join(rv.vars, greenness.vars, by = "RuelleID")
   
   # Buffer distances
-  b <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+  b <- c(50, 100)
   
-  # add NDSI
+  # select relevant buffers from the list
+  temp_plot <- temp_plot[c('buffer50', 'buffer100')]
+  
+  # add missing variables
   rv_na <- drop_na(rv)
   full_plot <- lapply(temp_plot, function(x){cbind(x, mean_NDSI = rv_na$mean_NDSI)})
   full_plot <- lapply(full_plot, function(x){cbind(x, Floral_coverage_per_m2 = rv_na$Floral_coverage_per_m2)})
   full_plot <- lapply(full_plot, function(x){cbind(x, Food_coverage_per_m2 = rv_na$Food_coverage_per_m2)})
-  
-  
+
   # Models ------------------------------------------------------
   
   # model
