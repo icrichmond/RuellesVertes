e8978eec5e264182ad998a83dab06f51edc223bb
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-03-07 10:32:50 -0500

    remove unnecessary code

--- _targets.R
+++ _targets.R
@@ -29,12 +29,6 @@ min_date <- as.POSIXct('2021-08-10', tz = "UTC")
 max_date <- as.POSIXct('2021-08-22', tz = "UTC")
 
 
-# Analysis ----------------------------------------------------------------
-source(file.path('script', 'sampling-selection-targets.R'))
-source(file.path('script', 'temperature-analysis-targets.R'))
-source(file.path('script', 'noise-analysis-targets.R'))
-source(file.path('script', 'model-targets.R'))
-
 # Targets: all ------------------------------------------------------------
 # Automatically grab all the "targets_*" lists above
 lapply(grep('targets', ls(), value = TRUE), get)
