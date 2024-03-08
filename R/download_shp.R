86b0e2685f76bebeee459a6236fa994ef3c96318
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-03-08 10:31:05 -0500

    change to unzip

--- R/download_shp.R
+++ R/download_shp.R
@@ -1,6 +1,13 @@
 download_shp <- function(url, dest){
+  
   temp <- tempfile()
-  download.file(url, dest, mode = "wb")
-  shp <- st_read(file.path("/vsizip", dest))
+  
+  download.file(url, temp, mode = "wb")
+  
+  unzip(temp, exdir = dest)
+  
+  shp <- st_read(dest)
+  
   return(shp)
+  
 }
