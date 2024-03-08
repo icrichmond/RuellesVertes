47a40008f3a9220105dce58164d489286719f6a6
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-03-08 15:15:29 -0500

    drop correct geometry

--- R/kml_final_sites.R
+++ R/kml_final_sites.R
@@ -4,7 +4,7 @@ kml_final_sites <- function(rv_sp, spt) {
   # join rv_sp to spt to save kml layer for google maps 
   rv_sp <- arrange(rv_sp, Uniq_S_ID)
   spt <- select(spt, -c(lat,lon))
-  spt_kml <- inner_join(rv_sp, st_drop_geometry(spt))
+  spt_kml <- inner_join(st_drop_geometry(rv_sp), spt)
   spt_kml <- st_as_sf(spt_kml)
   
   ### TODO NEED SPT
