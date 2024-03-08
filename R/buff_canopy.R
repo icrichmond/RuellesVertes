d94c5b87f0454c3fcbf3b423d05e2bf067aae628
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-03-08 12:11:23 -0500

    calculate per of each land cover type

--- R/buff_canopy.R
+++ R/buff_canopy.R
@@ -15,6 +15,30 @@ buff_canopy <- function(sens, cc) {
       st_as_sf() %>% 
       rename(percan = `660_IndiceCanopee_2019.tif`)
     
+    impgr <- aggregate(cc, x, FUN = function(y) sum(y == 1)/length(y)) %>% 
+      st_as_sf() %>% 
+      rename(perimpgr = `660_IndiceCanopee_2019.tif`)
+    
+    veggr <- aggregate(cc, x, FUN = function(y) sum(y == 3)/length(y)) %>% 
+      st_as_sf() %>% 
+      rename(perveggr = `660_IndiceCanopee_2019.tif`)
+    
+    build <- aggregate(cc, x, FUN = function(y) sum(y == 2)/length(y)) %>% 
+      st_as_sf() %>% 
+      rename(perbuild = `660_IndiceCanopee_2019.tif`)
+    
+    wat <- aggregate(cc, x, FUN = function(y) sum(y == 5)/length(y)) %>% 
+      st_as_sf() %>% 
+      rename(perwat = `660_IndiceCanopee_2019.tif`)
+    
+    df <- data.frame(RuelleID = x$RuelleID, 
+                     totarea = st_area(x$geometry),
+                     percan = can$percan,
+                     perimpgr = impgr$perimpgr,
+                     perveggr = veggr$perveggr,
+                     perbuild = build$perbuild,
+                     perwat = wat$perwat)
+    
     })%>%
     purrr::set_names(., nm = paste0("int", b))
   
