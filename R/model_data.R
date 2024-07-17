10989c8dfe157a42d41255c58760ec92cc2fdcce
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-07-17 10:17:36 -0400

    remove aesthetic models

--- R/model_data.R
+++ R/model_data.R
@@ -5,8 +5,7 @@ model_data <- function(temp_plot, rv, road_area, var){
   rv.vars <- rv %>% 
     select(c("RuelleID", "Food_coverage_per_m2", "Floral_coverage_per_m2",
              "mean_NDSI", "Avg_max_daily_temp", "Avg_min_daily_temp",
-             "Ruelle_length_m","Ruelle_area_m2", "Groundcover_avg", "Midstorey_avg", "Canopy_avg",
-             "PublicArt", "PlayEquipment","WildlifeSupport","MaintainedGardens","StructureCondition")) %>%
+             "Ruelle_length_m","Ruelle_area_m2", "Groundcover_avg", "Midstorey_avg", "Canopy_avg")) %>%
     mutate_if(is.character, as.factor)
   rv.vars$RuelleID <- gsub("_", "-", rv.vars$RuelleID)
   
@@ -54,17 +53,12 @@ model_data <- function(temp_plot, rv, road_area, var){
   veg <- lm(rv.vars.s[,var] ~ Groundcover_avg + Midstorey_avg + Canopy_avg + 
                   Ruelle_length_m + Ruelle_area_m2, data = rv.vars)
   
-
-  aes1 <- lm(rv.vars.s[,var] ~ PublicArt + PlayEquipment + StructureCondition, data = rv.vars)
-  
-  aes2 <- lm(rv.vars.s[,var] ~ WildlifeSupport + MaintainedGardens, data = rv.vars)
-  
   buff <- map(.x = full_plot_s, .f = function(x){lm(x[,var] ~ perveggr + perbuild + percan + road_area_m2, data = x)}) %>%
     set_names(., nm = paste0(var, "_", b))
   
   # diagnostics
-  win <- list(veg, aes1, aes2)
-  names(win) <- c(paste0("veg_", var), paste0("aes1_", var), paste0("aes2_", var))
+  win <- list(veg)
+  names(win) <- c(paste0("veg_", var))
   
   mod <- list(win, buff)
   mod_figs <- purrr::map(.x = mod, .f = function(x){imap(x, resid_plots)})
