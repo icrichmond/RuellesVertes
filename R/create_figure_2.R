bda8d91d9032c473749bf671aa560a12af36b08d
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-07-17 16:11:35 -0400

    start fig 2

--- R/create_figure_2.R
+++ R/create_figure_2.R
@@ -1,4 +1,9 @@
-create_figure_2 <- function(rv) {
+create_figure_2 <- function(rv, temp_plot) {
+  
+  b50 <- temp_plot$buffer50 |>
+    mutate(RuelleID = str_replace_all(RuelleID, "-", "_"))
+  
+  full <- left_join(rv, b50, by = "RuelleID")
   
   
   
