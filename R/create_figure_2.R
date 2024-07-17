7e578bb2cd78e46476d90827ea6227f5de4506e2
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-07-17 10:49:16 -0400

    combine fig 2 and fig 3 into new fig 3

--- R/create_figure_2.R
+++ R/create_figure_2.R
@@ -1,16 +1,5 @@
 create_figure_2 <- function(rv) {
   
-  floral <- lm(Floral_coverage_per_m2 ~ Groundcover_avg + Midstorey_avg + Canopy_avg + 
-                        Ruelle_length_m + Ruelle_area_m2, data = rv)
   
-  p <- plot_predictions(floral, condition = "Groundcover_avg") + 
-    geom_point(data = rv, aes(x = Groundcover_avg, y = Floral_coverage_per_m2)) + 
-    labs(x = "Ground cover vegetation (%)", y = expression("Floral Coverage ("*m^2*")")) + 
-    theme_classic() + 
-    theme(axis.text = element_text(size = 14),
-          legend.text = element_text(size = 14),
-          legend.title = element_text(size = 14))
-  
-  ggsave('graphics/Figure2.png', p, dpi = 450)
   
 }
--- R/create_figure_3.R
+++ R/create_figure_3.R
@@ -1,21 +1,39 @@
 create_figure_3 <- function(temp_plot, road_area, rv) {
   
+  floral <- lm(Floral_coverage_per_m2 ~ Groundcover_avg + Midstorey_avg + Canopy_avg + 
+                 Ruelle_length_m + Ruelle_area_m2, data = rv)
+  
+  p <- plot_predictions(floral, condition = "Groundcover_avg") + 
+    geom_point(data = rv, aes(x = Groundcover_avg, y = Floral_coverage_per_m2)) + 
+    labs(x = "Ground cover vegetation (%)", y = bquote(atop(Habitat~provision~`for`~pollinators~phantom(),
+                                                            (floral~coverage~m^2)))) + 
+    theme_classic() + 
+    theme(axis.text = element_text(size = 14, colour="black"),
+          axis.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
+          legend.title = element_text(size = 14))
+  
   temp_plot[['buffer50']]$road_area_m2 <- road_area[['buffer50']]$road_area_m2
   
   b50 <- temp_plot$buffer50
   rv$RuelleID <- gsub("_", "-", rv$RuelleID)
   df <- inner_join(b50, rv, by = "RuelleID")
+  df <- df %>% mutate(percan = percan*100)
   
   mod <- lm(mean_NDSI ~ perveggr + perbuild + percan + road_area_m2, data = df)
   
   can <- plot_predictions(mod, condition = c("percan")) + 
     geom_point(data = df, aes(x = percan, y = mean_NDSI)) + 
-    labs(x = "Percent Canopy Cover @ 50 m", y = "Mean NDSI") + 
+    labs(x = "Surrounding canopy cover within 50 m buffer (%)", y = "Mean NDSI value") + 
     theme_classic() + 
-    theme(axis.text = element_text(size = 14),
+    theme(axis.text = element_text(size = 14, colour = "black"),
+          axis.title = element_text(size = 14),
           legend.text = element_text(size = 14),
           legend.title = element_text(size = 14))
   
-  ggsave('graphics/Figure3.png', can, width = 10, height = 8, units = c('in'), dpi = 450)
+  full <- p / can + plot_annotation(tag_levels = 'a', tag_suffix = ")")
+  
+  
+  ggsave('graphics/Figure3.png', full, width = 10, height = 12, units = c('in'), dpi = 450)
     
 }
